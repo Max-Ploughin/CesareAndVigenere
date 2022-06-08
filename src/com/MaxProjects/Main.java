fb8ca37357ba8b1e6891479e0fb3725fbf16073c
Max-Ploughin <82418513+Max-Ploughin@users.noreply.github.com>
2022-06-08 17:56:13 +0300

    Update Main.java

--- src/com/MaxProjects/Main.java
+++ src/com/MaxProjects/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Cesare.toCesarCode("hello", 3));
-        System.out.println(Cesare.fromCesareCode("khoor", 33));
+        System.out.println(Cesare.fromCesareCode("khoor", 3));
 
         System.out.println(Vigenere.toVigenere("hello", "lem"));
         System.out.println(Vigenere.fromCodeVigenere("sixws", "lem"));
